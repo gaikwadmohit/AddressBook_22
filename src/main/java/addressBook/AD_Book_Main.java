479392f25c8986d0b93aa5391ce68bd7335d7cf6
unknown <mohit16gaikwad@gmail.com>
2022-04-18 10:05:29 +0530

    UC1: Added code to add a new contact list in address book

--- src/main/java/addressBook/AD_Book_Main.java
+++ src/main/java/addressBook/AD_Book_Main.java
@@ -5,71 +5,62 @@ import java.util.Scanner;
 import addressBook.PersonContact;
 
 public class AD_Book_Main {
-	/**
-	 *@PROBLEM_STATEMENT : UC1 Create Contact
-	 *@author Mohit Gaikwad
-	 *@Date 18-April-2022
-	 */
-	
-			public void createContact() {
+	public void createContact() {
 
-			PersonContact contact = new PersonContact();
-			Scanner sc = new Scanner(System.in);
+		PersonContact contact = new PersonContact();
+		Scanner sc = new Scanner(System.in);
 
-			System.out.print("\nEnter First Name  : ");
-			String firstName = sc.nextLine();
-			contact.setFirstName(firstName);
+		System.out.print("\nEnter First Name  : ");
+		String firstName = sc.nextLine();
+		contact.setFirstName(firstName);
 
-			System.out.print("\nEnter Last Name  : ");
-			String lastName = sc.nextLine();
-			contact.setLastName(lastName);
+		System.out.print("\nEnter Last Name  : ");
+		String lastName = sc.nextLine();
+		contact.setLastName(lastName);
 
-			System.out.print("\nEnter E-mail  : ");
-			String email = sc.nextLine();
-			contact.setEmail(email);
+		System.out.print("\nEnter E-mail  : ");
+		String email = sc.nextLine();
+		contact.setEmail(email);
 
-			System.out.print("\nEnter Address  : ");
-			String address = sc.nextLine();
-			contact.setAddress(address);
+		System.out.print("\nEnter Address  : ");
+		String address = sc.nextLine();
+		contact.setAddress(address);
 
-			System.out.print("\nEnter City  : ");
-			String city = sc.nextLine();
-			contact.setCity(city);
+		System.out.print("\nEnter City  : ");
+		String city = sc.nextLine();
+		contact.setCity(city);
 
-			System.out.print("\nEnter State  : ");
-			String state = sc.nextLine();
-			contact.setState(state);
+		System.out.print("\nEnter State  : ");
+		String state = sc.nextLine();
+		contact.setState(state);
 
-			System.out.print("\nEnter Phone Number  : ");
-			long phoneNo = sc.nextLong();
-			contact.setPhoneNo(phoneNo);
+		System.out.print("\nEnter Phone Number  : ");
+		long phoneNo = sc.nextLong();
+		contact.setPhoneNo(phoneNo);
 
-			System.out.print("\nEnter Zip  : ");
-			int zip = sc.nextInt();
-			contact.setZip(zip);
-			showContact(contact);
-			sc.close();
-
-		}
-
-		public void showContact(PersonContact contact) {
-
-			System.out.print("\n-----------------");
-			System.out.print("\nFirst Name  : " + contact.getFirstName());
-			System.out.print("\nLast Name   : " + contact.getLastName());
-			System.out.print("\nAddress     : " + contact.getAddress());
-			System.out.print("\nCity        : " + contact.getCity());
-			System.out.print("\nState       : " + contact.getState());
-			System.out.print("\nPhone Number : " + contact.getPhoneNo());
-			System.out.print("\nE-mail      : " + contact.getEmail());
-			System.out.print("\nZip         : " + contact.getZip());
-
-		}
-
-		public static void main(String[] args) {
-
-			AD_Book_Main book = new AD_Book_Main();
-			book.createContact();
-		}
+		System.out.print("\nEnter Zip  : ");
+		int zip = sc.nextInt();
+		contact.setZip(zip);
+		showContact(contact);
+		sc.close();
 	}
 
+	public void showContact(PersonContact contact) {
+
+		System.out.print("\n-----------------");
+		System.out.print("\nFirst Name  : " + contact.getFirstName());
+		System.out.print("\nLast Name   : " + contact.getLastName());
+		System.out.print("\nAddress     : " + contact.getAddress());
+		System.out.print("\nCity        : " + contact.getCity());
+		System.out.print("\nState       : " + contact.getState());
+		System.out.print("\nPhone Number : " + contact.getPhoneNo());
+		System.out.print("\nE-mail      : " + contact.getEmail());
+		System.out.print("\nZip         : " + contact.getZip());
+	}
+
+	public static void main(String[] args) {
+
+		AD_Book_Main book = new AD_Book_Main();
+		book.createContact();
+	}
+}
