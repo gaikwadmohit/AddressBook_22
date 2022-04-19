bab0d75f265fb86a49782a92aaae63da794ab156
unknown <mohit16gaikwad@gmail.com>
2022-04-19 15:00:00 +0530

    Added : Program for the view person by the city or city

--- src/main/java/addressBook/Main_AB.java
+++ src/main/java/addressBook/Main_AB.java
@@ -7,25 +7,23 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main_AB {
-	  static Scanner scan = new Scanner(System.in);
-
-	    public static void main(String[] args) {
-	        System.out.println("Welcome to Address Book System");
-	        Map<String, AD_Book_Main> map = new HashMap<>();
-	        System.out.println("Enter Number of address book you want to add");
-	        Integer count = scan.nextInt();
-	        Integer i = 0;
-	        while (i < count) {
-	            System.out.println("Enter  book name");
-	            String bookName = scan.next();
-	            AD_Book_Main addressBook = new AD_Book_Main();
-	            addressBook.operation();
-	            map.put(bookName, addressBook);
-	            i++;
-	        }
-	        System.out.println(map);
 
 
-	    }
+    static Scanner scan = new Scanner(System.in);
 
-	}
+    public static void main(String[] args) {
+    	System.out.println("Welcome to Address Book System");
+        Map<String, AD_Book_Main> map = new HashMap<>();
+        System.out.println("Enter Number of address book you want to add");
+        Integer count = scan.nextInt();
+        Integer i = 0;
+        while (i < count) {
+            System.out.println("Enter  book name");
+            String bookName = scan.next();
+            AD_Book_Main addressBook = new AD_Book_Main();
+            addressBook.operation();
+            map.put(bookName, addressBook);
+            i++;
+        }
+        System.out.println(map);
+    }}  
