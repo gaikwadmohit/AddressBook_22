9cf60427aed641b110a9616e6b917c32881c4773
unknown <mohit16gaikwad@gmail.com>
2022-04-19 14:49:52 +0530

    UC8: Ability to search person in a city or state across the multiple addressbook

--- src/main/java/addressBook/Main_AB.java
+++ src/main/java/addressBook/Main_AB.java
@@ -1,108 +1,31 @@
 package addressBook;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main_AB {
-	private static List<AD_Book_Main> addressBooks = new LinkedList<AD_Book_Main>();
-	private static String[] addressBookName = new String[10];
-	private static int numOfBooks = 0;
+	  static Scanner scan = new Scanner(System.in);
 
-	private boolean checkName(String name) {
-		for (int i = 0; i <addressBooks.size(); i++) {
-			if (addressBookName[i].equals(name))
-				return true;
-		}
-		return false;
+	    public static void main(String[] args) {
+	        System.out.println("Welcome to Address Book System");
+	        Map<String, AD_Book_Main> map = new HashMap<>();
+	        System.out.println("Enter Number of address book you want to add");
+	        Integer count = scan.nextInt();
+	        Integer i = 0;
+	        while (i < count) {
+	            System.out.println("Enter  book name");
+	            String bookName = scan.next();
+	            AD_Book_Main addressBook = new AD_Book_Main();
+	            addressBook.operation();
+	            map.put(bookName, addressBook);
+	            i++;
+	        }
+	        System.out.println(map);
+
+
+	    }
+
 	}
-
-	private static void addressMenu(AD_Book_Main addressBook) {
-		try (Scanner sc = new Scanner(System.in)) {
-			int option = 0;
-			boolean exit = true;
-			while (exit) {
-				System.out.println(
-						"Select option 1: add user.  2: edit existing user.  3: display all users 4:Delete contact. 5:Switch Address Book");
-				option = sc.nextInt();
-				switch (option) {
-				case 1:
-					System.out.println("added");
-					addressBook.addContacts();
-					break;
-				case 2:
-					System.out.println("Enter the first name to edit");
-					addressBook.edit();
-					break;
-				case 3:
-					System.out.println("display");
-					addressBook.display();
-					break;
-				case 4:
-					System.out.println("Enter name");
-					addressBook.delete();
-					break;
-				default:
-					exit = false;
-
-				}
-				System.out.println();
-			}
-		}
-	}
-
-	public static void main(String[] args) {
-		System.out.println("Welcome to address book program");
-		Scanner sc = new Scanner(System.in);
-
-		AD_Book_Main currentBook;
-		int choice = 0;
-		boolean exit1 = true;
-		while (exit1) {
-			System.out.println("Select option 1:Add address Book 2:open Address Book 4:Display 5:exit");
-			choice = sc.nextInt();
-			switch (choice) {
-			case 1:
-				System.out.println("Enter the address book name");
-				String name = sc.next();
-				currentBook = new AD_Book_Main();
-				addressBooks.add(currentBook);
-				addressBookName[numOfBooks] = name;
-				numOfBooks++;
-				break;
-			case 2:
-				System.out.println("The Address books available :");
-				for (int i = 0; i < numOfBooks; i++) {
-					System.out.println(addressBookName[i]);
-				}
-				System.out.println("Enter the address book name");
-				String bookName = sc.next();
-				int i = 0;
-				for (i = 0; i < numOfBooks; i++) {
-					if (addressBookName[i].equals(bookName))
-						break;
-				}
-
-				if (i == numOfBooks) {
-					System.out.println("name Not Found");
-					break;
-				}
-				currentBook = addressBooks.get(i);
-				addressMenu(currentBook);
-				break;
-			case 4:
-				printAdressBooks();
-			default:
-				exit1 = false;
-			}
-		}
-		sc.close();
-	}
-
-	private static void printAdressBooks() {
-		for (String adressBooksDisplay : addressBookName) {
-			System.out.println(adressBooksDisplay);
-		}
-	}
-}
-
