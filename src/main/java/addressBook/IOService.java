24746b4dc919a55ca8b015d5143492d90052c536
unknown <mohit16gaikwad@gmail.com>
2022-04-25 17:14:22 +0530

    Added: Ability to read write AD.Book with person contact in file using file IO

--- src/main/java/addressBook/IOService.java
+++ src/main/java/addressBook/IOService.java
@@ -2,8 +2,6 @@ package addressBook;
 
 public class IOService {
 
-	public static final IOService LIST_DS_IO = null
-	
+	public static IOService LIST_DS_IO;
 	 TXT_FILE_IO, LIST_DS_IO, CSV_IO, JSON_IO
-	}
-
+}
--- src/main/java/com/bridgelabz/addressbook/IOService.java
+++
@@ -1,5 +0,0 @@
-package com.bridgelabz.addressbook;
-
-public enum IOService {
-    TXT_FILE_IO, LIST_DS_IO, CSV_IO, JSON_IO
-}
